dbb928199d851f18b2fdf17fa094cfb2e37fe1c8
HarshilGem <jetaniharshil0810@gmail.com>
2025-08-19 13:59:33 +0530

    Hi

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ const path = require('path');
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'client', 'build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+// });
 
 const userRouter = require('./Routes/user');
 app.use('/api/users', userRouter);
